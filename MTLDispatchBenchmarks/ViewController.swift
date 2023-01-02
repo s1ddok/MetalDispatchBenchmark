994e096f7bbfe094a7ca1810bc885306be44b002
Andrey Volodin <siddok@gmail.com>
2023-01-03 03:52:31 +0600

    update to modern alloy and improve accuracy

--- MTLDispatchBenchmarks/ViewController.swift
+++ MTLDispatchBenchmarks/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Alloy
 
+typealias BenchmarkResult = (TimeInterval, Configuration)
+
 extension MTLSize: Hashable, CustomDebugStringConvertible {
     public static func == (lhs: MTLSize, rhs: MTLSize) -> Bool {
         return lhs.width == rhs.width && lhs.height == rhs.height && lhs.depth == rhs.depth
@@ -42,12 +44,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let context = MTLContext(device: Metal.device)
+        let context = try! MTLContext(device: Metal.device)
 
         self.context = context
         
         guard
-            let library = context.standardLibrary,
+            let library = try? self.context.library(for: ViewController.self),
             let exactPipelineState = try? library.computePipelineState(function: "exactIncrement"),
             let uniformPipelineState = try? library.computePipelineState(function: "uniformIncrement")
         else {
@@ -70,8 +72,8 @@ class ViewController: UIViewController {
         }
 
 
-        let texture1 = context.texture(width: 513, height: 513, pixelFormat: .rgba32Sint, usage: [.shaderRead, .shaderWrite])
-        let texture2 = context.texture(width: 513, height: 513, pixelFormat: .rgba32Sint, usage: [.shaderRead, .shaderWrite])
+        let texture1 = try! context.texture(width: 513, height: 513, pixelFormat: .rgba32Sint, usage: [.shaderRead, .shaderWrite])
+        let texture2 = try! context.texture(width: 513, height: 513, pixelFormat: .rgba32Sint, usage: [.shaderRead, .shaderWrite])
 
         self.texture1 = texture1
         self.texture2 = texture2
@@ -85,62 +87,83 @@ class ViewController: UIViewController {
         print(self.optimizedUniformState.threadExecutionWidth)
     }
 
-    var results: [(TimeInterval, Configuration)] = []
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-
+        
+        var top10s = [[(Double, String, MTLSize)]]()
+        
+        for i in 0..<1 {
+            let results = self.run()
+            
+            let normalizedResults = results.map { ($0.0 / results.first!.0, $0.1.pipelineState, $0.1.threadgroupSize) }
+            normalizedResults.forEach { print($0) }
+            
+            print("--- GLOBAL NORMALIZED END")
+            let groupedByThreadgroup = Dictionary(grouping: results) { $0.1.threadgroupSize }
+        
+            for (key, results) in groupedByThreadgroup {
+                print("Best results for key: \(key)")
+                print(results.sorted { $0.0 < $1.0 }.map { ($0.0, $0.1.pipelineState) }[0..<3])
+            }
+            
+            top10s.append(Array(normalizedResults.prefix(10)))
+        }
+        
+        top10s.forEach {
+            print("--------------------------")
+            $0.forEach { print($0) }
+        }
+    }
+    
+    func run() -> [BenchmarkResult] {
         let iterations = 400
         
-        results.removeAll()
+        var results: [(TimeInterval, Configuration)] = []
         for _ in 0...20 {
             let possibleDivisors = (0...5).map { Int(pow(2.0, Double($0))) }.shuffled()
             
             for d in possibleDivisors {
                 var dispatchGroupSize = self.uniformState.max2dThreadgroupSize
                 dispatchGroupSize.height /= d
-                self.testDispatch(configuration: Configuration(threadgroupSize: dispatchGroupSize,
-                                                               pipelineState: "uniform"),
-                                  iterations: iterations)
-                /*self.testDispatch(configuration: Configuration(threadgroupSize: dispatchGroupSize,
-                                                               pipelineState: "uniformOptimized"),
-                                  iterations: iterations)*/
-                self.testDispatch(configuration: Configuration(threadgroupSize: dispatchGroupSize,
-                                                               pipelineState: "exact"),
-                                  iterations: iterations)
+                
+                var calls = [() -> [BenchmarkResult]]()
+                calls.append( {
+                    self.testDispatch(configuration: Configuration(threadgroupSize: dispatchGroupSize,
+                                                                   pipelineState: "uniform"),
+                                      iterations: iterations)
+                })
+                
+                calls.append({
+                    self.testDispatch(configuration: Configuration(threadgroupSize: dispatchGroupSize,
+                                                                   pipelineState: "uniformOptimized"),
+                                      iterations: iterations)
+                })
+                    
+                calls.append({
+                    self.testDispatch(configuration: Configuration(threadgroupSize: dispatchGroupSize,
+                                                                   pipelineState: "exact"),
+                                      iterations: iterations)
+                })
+                
+                calls.lazy.shuffled().map { $0() }.forEach { results.append(contentsOf: $0) }
             }
         }
-
-        //try? context.schedule { buffer in
-        //    buffer.addCompletedHandler { _ in
-                print(self.results.map { $0.0 })
-                
-                print("---- TIME END ---")
-                
-                let sortedResults = self.results.sorted { $0.0 < $1.0 }
-
-                let normalizedResults = sortedResults.map { ($0.0 / sortedResults.first!.0, $0.1.pipelineState, $0.1.threadgroupSize) }
-                print(normalizedResults)
-                
-                print("--- GLOBAL NORMALIZED END")
-                let groupedByThreadgroup = Dictionary(grouping: self.results) { $0.1.threadgroupSize }
-            
-                for (key, results) in groupedByThreadgroup {
-                    print("Best results for key: \(key)")
-                    print(results.sorted { $0.0 < $1.0 }.map { ($0.0, $0.1.pipelineState) }[0..<3])
-                }
-         //   }
-        //}
+        
+        print("---- TIME END ---")
+        
+        return results.sorted { $0.0 < $1.0 }
     }
 
-    func testDispatch(configuration: Configuration, iterations: Int) {
+    func testDispatch(configuration: Configuration, iterations: Int) -> [BenchmarkResult] {
+        var results: [BenchmarkResult] = []
         try? self.context.scheduleAndWait { buffer in
             buffer.compute { encoder in
                 for i in 1...iterations {
                     let t1 = i % 2 == 0 ? self.texture1 : self.texture2
                     let t2 = i % 2 == 0 ? self.texture2 : self.texture1
 
-                    encoder.set(textures: [t1, t2])
+                    encoder.setTextures(t1, t2)
 
                     let state: MTLComputePipelineState
 
@@ -163,10 +186,12 @@ class ViewController: UIViewController {
                 }
 
                 buffer.addCompletedHandler { _buffer in
-                    self.results.append((_buffer.gpuExecutionTime, configuration))
+                    results.append((_buffer.gpuExecutionTime, configuration))
                 }
             }
         }
+        
+        return results
     }
 
 
